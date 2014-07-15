19068e542ead547e893027b34a66504baadbaebb
Tiago Celestino <tcelestino@gmail.com>
2014-07-15 17:03:50 -0300

    change title

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/css/style.css?<?php echo md5(date('di')); ?>">
-  <title>WordPress Starter</title>
+  <title>WP Theme Starter</title>
   <?php wp_head(); ?>
   <script src="<?php bloginfo('template_directory'); ?>/assets/js/modernizr.min.js"></script>
 </head>
