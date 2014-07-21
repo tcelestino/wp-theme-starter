12f8969c196e9c3ea040348b1eaf1e82b3b8f318
Tiago Celestino <tcelestino@gmail.com>
2014-07-21 14:43:47 -0300

    markup to search page

--- search.php
+++ search.php
@@ -1,2 +1,25 @@
 <?php get_header(); ?>
+  <h2>Search Result</h2>
+  <?php if(have_posts()) { ?>
+  <dl class="result-search">
+    <?php while(have_posts()) { the_post(); 
+
+      $postxt = ($post->excerpt == '') : $post->post_content ? $post->post_excerpt;
+
+    ?>
+    <dt class="title">
+      <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+        <?php the_title(); ?>
+      </a>
+    </dt>
+    <dd class="description">
+      <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+        <?php getTruncateText($postxt, 100); ?>
+      </a>
+    </dd>
+    <?php } ?>
+  </dl>
+  <?php } else { ?>
+  <p><?php echo __e('Nothing result.'); ?></p>
+  <?php } ?>
 <?php get_footer(); ?>
