a3c3550d3108e7b038399eb65767bb750c60bfc9
Tiago Celestino <tcelestino@gmail.com>
2014-11-18 14:49:16 -0200

    add browser sync socket.io

--- footer.php
+++ footer.php
@@ -1,5 +1,12 @@
   <?php wp_footer(); ?>
+
   <script src="<?php bloginfo('template_directory'); ?>/assets/js/vendor.js"></script>
   <script src="<?php bloginfo('template_directory'); ?>/assets/js/app.js"></script>
+
+  <script src='//10.110.60.60:3000/socket.io/socket.io.js'></script>
+  <script>var ___socket___ = io.connect('http://10.110.60.60:3000');</script>
+  <script src='//10.110.60.60:3001/client/browser-sync-client.0.7.7.js'></script>
+
+
 </body>
 </html>
